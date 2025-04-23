8bbfd4551d5b0877ec701a1999c091efbe696d6b
vivek kovi <vivekkovi93@gmail.com>
2025-04-23 13:10:43 -0500

    updated bot

--- bot.py
+++ bot.py
@@ -1,3 +1,14 @@
+# This code will act as a Twitter bot that fetches recent tweets containing specific hashtags.
+# It will use the Twitter API v2 to search for tweets and print their URLs.
+# It will also handle rate limits by waiting for the appropriate time before making another request.
+# It requires the `requests` library for making HTTP requests and `python-dotenv` for loading environment variables.
+# It will also use the `datetime` library to handle time-related operations.
+# It will run indefinitely until it successfully fetches tweets or encounters an error.
+# It will print the URLs of the tweets in the format "https://twitter.com/{username}/status/{tweet_id}".
+# It will also print the error message if the request fails for any reason.
+# It will wait for the rate limit to reset if it encounters a 429 status code.
+# It will also print the wait time before making another request.
+# Make sure you have .env file associated with TWITTER_BEARER_TOKEN It will use the `dotenv` library to load the Twitter Bearer Token from a `.env` file.
 import os
 import requests
 from dotenv import load_dotenv
@@ -9,7 +20,8 @@ load_dotenv()
 BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
 
 SEARCH_URL = "https://api.twitter.com/2/tweets/search/recent"
-SEARCH_QUERY = "#XRP OR #Bitcoin -is:retweet lang:en"
+SEARCH_QUERY = "#XRP OR #Bitcoin -is:retweet lang:en" # CHANGE THIS TO YOUR DESIRED QUERY BY INCLUDING HASHTAGS
+# Example: "#XRP OR #Bitcoin -is:retweet lang:en" will search for tweets containing either #XRP or #Bitcoin, excluding retweets, in English.
 MAX_RESULTS = 10
 
 def fetch_tweets():
